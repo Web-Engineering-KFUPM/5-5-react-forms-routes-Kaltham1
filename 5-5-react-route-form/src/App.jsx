f02cd8b050c242dc0352562143088bef065eb994
Kaltham Ahashmi <s202371470@kfupm.edu.sa>
2026-03-02 13:06:27 +0300

    TODO1

--- 5-5-react-route-form/src/App.jsx
+++ 5-5-react-route-form/src/App.jsx
@@ -144,24 +144,45 @@
 // - Use the hints above to guide your implementation, but write the actual JSX and logic yourself.
 // ================================================================
 
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Registration from "./pages/Registration";
 export default function App() {
   return (
+    <BrowserRouter>
     <div className="app">
       <nav className="navbar">
         <div className="brand">🧑‍💻 Student Portal</div>
         <div className="links">
-          {/*Nav links*/}
+          <NavLink to="/" end className="navlink">
+            Home
+          </NavLink>
+
+          <NavLink to="/about" className="navlink">
+            About
+          </NavLink>
+
+          <NavLink to='/registration' className="navlink">
+            Registration 
+          </NavLink>
         </div>
       </nav>
 
       <main className="container">
-        {/*Routes*/}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="*" element={<h2>404 - Page Not Found</h2>} />
+        </Routes>
       </main>
 
       <footer className="footer">
         <span>© {new Date().getFullYear()} React Student Portal</span>
       </footer>
     </div>
+    </BrowserRouter>
   );
 }
